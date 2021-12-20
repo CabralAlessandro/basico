6498f1f8801984b468df4c4a233a962f4a7f4ff3
conrado-rosa <conradomr@protonmail.com>
2021-12-20 16:01:00 -0300

    Exercício 1

---
+++ Conrado Rosa/Exercícios rápidos/exercicio-1.js
@@ -0,0 +1,11 @@
+/*
+function calcularMedia(nota1, nota2, nota3) {
+    return ((nota1 + nota2 + nota3) / 3);
+}
+console.log(calcularMedia(8.2, 7.8, 2.3));
+*/
+
+function calcularCelsius(C) {
+    return ((C * 1.8) + 32); // calcula em Fahrenheit
+}
+console.log(calcularCelsius(60));
