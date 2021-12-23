29be1430979c0b3a40bbf75057d73c1df653fe31
ita0910 <itamar_perolim@hotmail.com>
2021-12-23 17:41:35 -0300

    exercicio rápido 1
    criei uma pasta dentro da minha pasta com o nome Exercicios rapidos, criei um programa para transformar C° em F°, criei um arquivo de testes para testar possibilidades de codigos.

---
+++ Itamar Perolin/Exercicios rápidos/exercicio-1.js
@@ -0,0 +1,4 @@
+var grausC = 30; //declara o valor em graus celsius
+var grausF = ( ((grausC)*1.8)+32) // declara uma variavel que calcula graus fahrenheit em função de graus celsius
+
+console.log(grausC,'graus celcius =', grausF,'graus fahrenheit.' ) //printa o valor de graus celsius convertido em fahrenheit
---
+++ Itamar Perolin/Exercicios rápidos/teste1.js
@@ -0,0 +1,13 @@
+function calcularCelsius(C) {
+    return ((C * 1.8) + 32); // calcula em Fahrenheit
+}
+console.log(calcularCelsius(30));
+
+/*
+
+//ex:
+function calcularMedia(nota1, nota2, nota3) {
+    return ((nota1 + nota2 + nota3) / 3);
+}
+console.log(calcularMedia(8.2, 7.8, 2.3));
+*/
