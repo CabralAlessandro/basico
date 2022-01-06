60850479c8fb720d443c5ca3e35ac558e7093568
Alessandro <alessandro.cabral@ebinterchange.com.br>
2022-01-06 09:17:12 -0300

    Tarefa 1 Alessandro

---
+++ Alessandro Cabral/Main.js
@@ -0,0 +1,17 @@
+/*
+Olá,
+Eu já conheço um pouco de programação, mas não aprendi a trabalhar com github.
+Meu objetivo aqui é aprender a trabalhar com equipes e tornar meus códigos mais
+parecidos com um discurso e menos parecidos com o fruto de uma patologia.
+*/
+
+var valor = 207.468;
+var nome = "Alessandro"
+
+function imprimeValores(valor, variavel_nome){
+    console.log("O valor enviado é:", valor)
+    //Impressão do meu nome
+    console.log("O nome enviado é:", variavel_nome)
+}
+
+imprimeValores(valor, nome);
